e3f0600be1be15ce75aad716e44cf93d00b9ad99
rikito <rikito1036@icloud.com>
2021-03-23 06:22:05 +0900

    .once @click=v-on:click

--- if/main.js
+++ if/main.js
@@ -1,8 +1,31 @@
 const app = new Vue({
   el: '#app',
+  /*  data: {
+     message: ''
+   }, */
   data: {
-    toggle: false,
-    color: 'yel'
+    message: ''
+  },
+  //メソッドイベントハンドラー
+  methods: {
+    clickHandler: function () {
+      alert('click');
+    }
+    /*  clear: function () {
+       this.message = ''
+     },
+     clickH: function () {
+       this.message = new Date().toLocaleTimeString()
+     },
+     //イベントオブジェクトの参照について
+     // $eventはvue.jsが定義している
+     clickHandler: function ($event, message) {
+       console.log($event);
+       this.message = message
+       // console.log(e.target.tagName);
+       /*  console.log(e.target.innerHTML)
+        console.log(e.target.type) */
+    /* this.counter++ */
   }
 })
 
