a8569459a3fd0b3907314df29b5243f7957fe8c2
rikito <rikito1036@icloud.com>
2021-03-23 04:51:15 +0900

    class binding

---
+++ class/main.js
@@ -0,0 +1,28 @@
+/* const app = new Vue({
+  el: '#app',
+  data: {
+    classObject: {
+      large: true,
+      'text-danger': true
+    }
+  },
+});
+
+ */
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    largeClass: {
+      large: true,
+      'bg-gray': true
+    },
+    dangerClass: {
+      'text-danger': true
+    },
+    isLarge: true
+  }
+})
+// 三項演算子
+
+
